d8bf71ce95e254adff06606bdc0e9c81635db0c2
Daniel Schnurpfeil <117296223+danschnurp@users.noreply.github.com>
2025-02-14 20:31:47 +0100

    WIP: script for sequenced dataset

--- dataset/prepare_coco_format.py
+++ dataset/prepare_coco_format.py
@@ -1,5 +1,10 @@
 import sys
+import time
 
+import yaml
+import random
+
+import numpy as np
 from ultralytics import YOLO
 import cv2
 import os
@@ -7,122 +12,152 @@ import argparse
 
 import json
 
-from utils.general_utils import download_video
+from utils.phash_similarity import calculate_pictures_similarity
+from utils.general_utils import download_video , normalize_text
+from utils.image_utils import get_roi_coordinates
+
+def euclidean_distance(a, b):
+    return np.sqrt(np.sum((np.array(a) - np.array(b))**2))
+
 
 parser = argparse.ArgumentParser(description='')
 
-parser.add_argument('--nett_name', default="yolov5mu.pt")
-parser.add_argument('--sequences_jsom_path', default="../traffic_lights.json")
-parser.add_argument('--sequence_seconds_before', type=float, default=0.002)
-parser.add_argument('--sequence_seconds_after', type=float, default=0.002)
-parser.add_argument('--work-dir', default="/Volumes/zalohy/dip")
-parser.add_argument('--label-light', type=int, default=80)
-parser.add_argument('--train-test-split', type=int, default=0.75)
+parser.add_argument('--nett_name', default="../yolov5mu.pt")
+parser.add_argument('--sequences_jsom_path', default="../railway_datasets/video_names.json")
+parser.add_argument('--in-dir', default="../reconstructed/all_yolov5mu_raw")
+parser.add_argument('--out-dir', default="../dataset")
+parser.add_argument('--label-light', type=int, default=79)
+parser.add_argument('--train-test-split', type=int, default=0.25)
 
 args = parser.parse_args()
 
 # where to save
-SAVE_PATH = args.work_dir
+SAVE_PATH = args.out_dir
+
+czech_railway_folder = "czech_railway_dataset_4_classes"
+classes_dir_path = "../railway_datasets/4_classes"
+dataset_yaml = '../metacentrum/CRTL_multi_4_labeled.yaml'
 
 
-czech_railway_folder = "czech_railway_dataset"
 img_index = 0
 
+with open(dataset_yaml, encoding="utf-8") as f:
+    class_mapping = yaml.load(f, Loader=yaml.SafeLoader)
+
+class_mapping = class_mapping["names"]
+class_mapping = dict(zip(class_mapping.values(), class_mapping.keys()))
 
 with open(args.sequences_jsom_path, encoding="utf-8", mode="r") as f:
-    traffic_lights = dict(json.load(f))
+    video_names = dict(json.load(f))
+    video_names = video_names["names"]
 
-del traffic_lights["names"]
-del traffic_lights["todo"]
+original_label = 'traffic light'
 
-interesting_labels = {'traffic light'}
+
 label_light = args.label_light
 
 # creating folder with video name
 if czech_railway_folder not in os.listdir(SAVE_PATH):
     os.mkdir(f"{SAVE_PATH}/{czech_railway_folder}")
-    for i in interesting_labels:
-        os.mkdir(f"{SAVE_PATH}/{czech_railway_folder}/train/")
-        os.mkdir(f"{SAVE_PATH}/{czech_railway_folder}/val/")
-        os.mkdir(f"{SAVE_PATH}/{czech_railway_folder}/train/images/")
-        os.mkdir(f"{SAVE_PATH}/{czech_railway_folder}/train/labels/")
-        os.mkdir(f"{SAVE_PATH}/{czech_railway_folder}/train/images/{i}/")
-        os.mkdir(f"{SAVE_PATH}/{czech_railway_folder}/train/labels/{i}")
-        os.mkdir(f"{SAVE_PATH}/{czech_railway_folder}/val/images/")
-        os.mkdir(f"{SAVE_PATH}/{czech_railway_folder}/val/labels/")
-        os.mkdir(f"{SAVE_PATH}/{czech_railway_folder}/val/images/{i}/")
-        os.mkdir(f"{SAVE_PATH}/{czech_railway_folder}/val/labels/{i}")
+    os.mkdir(f"{SAVE_PATH}/{czech_railway_folder}/train/")
+    os.mkdir(f"{SAVE_PATH}/{czech_railway_folder}/val")
+    os.mkdir(f"{SAVE_PATH}/{czech_railway_folder}/train/images")
+    os.mkdir(f"{SAVE_PATH}/{czech_railway_folder}/val/images")
+    os.mkdir(f"{SAVE_PATH}/{czech_railway_folder}/train/labels")
+    os.mkdir(f"{SAVE_PATH}/{czech_railway_folder}/val/labels")
 
 
+for i in ["multi_class"]:
+        if i not in os.listdir(f"{SAVE_PATH}/{czech_railway_folder}/train/images/"):
+            os.mkdir(f"{SAVE_PATH}/{czech_railway_folder}/train/images/{i}/")
+            os.mkdir(f"{SAVE_PATH}/{czech_railway_folder}/train/labels/{i}")
+            os.mkdir(f"{SAVE_PATH}/{czech_railway_folder}/val/images/{i}/")
+            os.mkdir(f"{SAVE_PATH}/{czech_railway_folder}/val/labels/{i}")
+
+all_classes = {}
+
+for i in os.listdir(classes_dir_path):
+    with open(classes_dir_path + "/" + i, encoding="utf-8") as f:
+        data = dict(json.load(f))["data"]
+    for j in data:
+        try:
+            all_classes[j["ytlink"]][j["timestamp in video"]].append(f"{class_mapping[j['color']]} {j['roi coordinates']}")
+        except KeyError:
+            try:
+                all_classes[j["ytlink"]][j["timestamp in video"]] = [f"{class_mapping[j['color']]} {j['roi coordinates']}"]
+            except KeyError:
+                all_classes[j["ytlink"]] = {
+                    j["timestamp in video"]: [f"{class_mapping[j['color']]} {j['roi coordinates']}"]
+                }
+
+
+total_pictures_count = sum([len([j for j in all_classes[i]]) for i in all_classes])
+last_train_sample = int(total_pictures_count * args.train_test_split)
+model = YOLO(args.nett_name)  # load an official model
 image_counter = 0
-for i in traffic_lights:
-    image_counter += len(i)
+#
+# with open("today_results.json", "w", encoding="utf-8") as f:
+#     json.dump(all_classes, f, indent=2, ensure_ascii=False)
+#
+# exit(0)
 
-last_train_sample = int(image_counter * args.train_test_split)
+lost_pictures = 0
+random.shuffle(all_classes)
+for video_link in all_classes:
+    timestamps_shuffled = list(all_classes[video_link].keys())
+    random.shuffle(timestamps_shuffled)
+    d_video = download_video(video_link, "../videos")
+    # todo detection, roi difference, x seconds before/after
+    for timestamp in timestamps_shuffled:
+        process_frame(lost_pictures, image_counter, img_index)
 
-image_counter = 0
 
-for i in traffic_lights:
-    d_video = download_video(i, SAVE_PATH)
-    # Load a model
-    model = YOLO(args.nett_name)  # load an official model
+print("lost/total", lost_pictures, "/", total_pictures_count)
 
-    # Load video
-    video_path = SAVE_PATH + '/' + d_video
+def process_frame(lost_pictures, image_counter, img_index):
+    previous_img = None
+    # todo loop
+    cap = cv2.VideoCapture(d_video)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    frame_number = int(fps * timestamp)
+    cap.set(cv2.CAP_PROP_POS_FRAMES,
+            frame_number)
+    _, frame = cap.read()
+    hamming_dist = calculate_pictures_similarity(frame, previous_img)
+    previous_img = frame
+    if hamming_dist < 3:
+        return
+    most_similar_quadruples = []
+    for original in all_classes[video_link][timestamp]:
+        detected = get_roi_coordinates(model, frame=frame)
+        original_split = list(map(float, original[original.find(" "):].split()))
+        # Calculate distances
+        distances = [euclidean_distance([original_split[:2], original_split[2:]], quad) for quad in detected]
+        if len(distances) == 0:
+            print("strange", file=sys.stderr)
+            lost_pictures += 1
+            continue
+        # Find the most similar quadruple
+        # todo check satisfaction distance
+        print(distances)
+        most_similar_index = np.argmin(distances)
+        detected[most_similar_index] = [*detected[most_similar_index][0], *detected[most_similar_index][1]]
+        most_similar_quadruples.append(
+            f"{original[:original.find(' ')]} {detected[most_similar_index][0]} {detected[most_similar_index][1]}"
+            f" {detected[most_similar_index][2]} {detected[most_similar_index][3]}\n")
+    all_classes[video_link][timestamp] = most_similar_quadruples
 
-    for seek_seconds in traffic_lights[i]:
-        image_counter += 1
-        video_name = d_video
-        #
-        cap = cv2.VideoCapture(video_path)
-        start_time = seek_seconds - args.sequence_seconds_before
-        print("starting from", seek_seconds, file=sys.stderr)
-        if start_time < 0.:
-            print("starting from beginning", file=sys.stderr)
-            start_time = 0
-        fps = cap.get(cv2.CAP_PROP_FPS)
-        frame_number = int(fps * start_time)
-        cap.set(cv2.CAP_PROP_POS_FRAMES,
-                frame_number)
-
-        while cap.isOpened():
-            ret, frame = cap.read()
-            if not ret:
-                break
-            # end of sequence
-            if (cap.get(cv2.CAP_PROP_POS_MSEC) / 1000.) > (args.sequence_seconds_after + seek_seconds):
-                print(f"finished {seek_seconds}\n", file=sys.stderr)
-                break
-            else:
-                # timestamp seconds from video beginning
-                timestamp = f"{float(cap.get(cv2.CAP_PROP_POS_MSEC) / 1000.):.3f}"
-                results = model.predict(frame)
-                # Iterate over the results
-                for result in results:
-                    boxes = result.boxes  # Boxes object for bbox outputs
-                    class_indices = boxes.cls  # Class indices of the detections
-                    class_names = [result.names[int(i)] for i in class_indices]  # Map indices to names
-                    print(class_names, "timestamp:", timestamp)
-                    if len(interesting_labels & set(class_names)) > 0:
-                        # saves the result
-
-                        if image_counter > last_train_sample:
-                            save_name = f"{SAVE_PATH}/{czech_railway_folder}/val/"
-                        else:
-                            save_name = f"{SAVE_PATH}/{czech_railway_folder}/train/"
-                        cv2.imwrite(
-                            f"{save_name}images/{list(interesting_labels & set(class_names))[0]}/{img_index}.jpg",
-                            frame)
-                        for r in results:
-                            boxes = r.boxes
-                            with open(
-                                    f"{save_name}labels/{list(interesting_labels & set(class_names))[0]}/{img_index}.txt",
-                                    mode="w") as label_f:
-                                for index, box in enumerate(boxes):
-                                    b = box.xyxy[0]  # get box coordinates in (left, top, right, bottom) format
-                                    if result.names[int(box.cls)] in interesting_labels:
-                                        label_f.write(f"{label_light} {box.xywhn.tolist()[0][0]} {box.xywhn.tolist()[0][1]}"
-                                                  f" {box.xywhn.tolist()[0][2]} {box.xywhn.tolist()[0][3]}\n")
-                        img_index += 1
-
-        cap.release()
+    if image_counter > last_train_sample:
+        save_name = f"{SAVE_PATH}/{czech_railway_folder}/val/"
+    else:
+        save_name = f"{SAVE_PATH}/{czech_railway_folder}/train/"
+    cv2.imwrite(f"{save_name}images/multi_class/{img_index}.jpg", frame)
+    with open(
+            f"{save_name}labels/multi_class/{img_index}.txt",
+            mode="w") as label_f:
+        coordinates = ""
+        for ii in all_classes[video_link][timestamp]:
+            coordinates += f"{ii}\n"
+        label_f.write(coordinates)
+    img_index += 1
+    image_counter += 1
