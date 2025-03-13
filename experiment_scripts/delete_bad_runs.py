80eabf7785914f864e47ab17eee5dd8a0692ce11
danschnurp <117296223+danschnurp@users.noreply.github.com>
2025-03-13 08:22:34 +0100

    trdetr,yolo grid search

---
+++ experiment_scripts/delete_bad_runs.py
@@ -0,0 +1,23 @@
+
+
+
+import os
+import shutil
+
+def remove_directories_without_results_png(base_path):
+    # Walk through the directory tree
+    for root, dirs, files in os.walk(base_path, topdown=False):
+        # Skip the base directory itself
+        if root == base_path:
+            continue
+        # Check if 'results.png' is in the current directory
+        if 'results.png' not in files:
+            # Remove the directory if it does not contain 'results.png'
+            print(f"Removing directory: {root}")
+            shutil.rmtree(root)
+
+# Define the base path
+base_path = '../metacentrum/done_experiments/rtdetr_yolo'
+
+# Call the function to remove directories
+remove_directories_without_results_png(base_path)
